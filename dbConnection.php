72e8c961e7f9a4e54e441cda31edebba63679be5
Neeraj saini <33536675+NeerajSaini-Dev@users.noreply.github.com>
2021-08-02 02:00:35 -0700

    Main Project Uploads
    Main project files uploaded.

---
+++ dbConnection.php
@@ -0,0 +1,7 @@
+<?php
+//all the variables defined here are accessible in all the files that include this one
+//$con= new mysqli('localhost','root','','project')or die("Could not connect to mysql".mysqli_error($con));
+$con=mysqli_connect("161.97.175.98","haxways_clg","WK;d-}g9S)Uk","haxways_clg");
+
+
+?>
